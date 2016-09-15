b1e74166dc4b54c1b51429f270d9f495d29b863a
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-09-15 14:49:25 -0700

    Remove extraneous log statement.

--- ios/RNNetworkInfo.m
+++ ios/RNNetworkInfo.m
@@ -20,33 +20,32 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_METHOD(getSSID:(RCTResponseSenderBlock)callback)
 {
     NSArray *interfaceNames = CFBridgingRelease(CNCopySupportedInterfaces());
-    NSLog(@"%s: Supported interfaces: %@", __func__, interfaceNames);
-    
+
     NSDictionary *SSIDInfo;
     NSString *SSID = @"error";
-    
+
     for (NSString *interfaceName in interfaceNames) {
         SSIDInfo = CFBridgingRelease(CNCopyCurrentNetworkInfo((__bridge CFStringRef)interfaceName));
-        
+
         if (SSIDInfo.count > 0) {
             SSID = SSIDInfo[@"SSID"];
             break;
         }
     }
-    
+
     callback(@[SSID]);
 }
 
 RCT_EXPORT_METHOD(getIPAddress:(RCTResponseSenderBlock)callback)
 {
     NSString *address = @"error";
-    
+
     struct ifaddrs *interfaces = NULL;
     struct ifaddrs *temp_addr = NULL;
     int success = 0;
-    
+
     success = getifaddrs(&interfaces);
-    
+
     if (success == 0) {
         temp_addr = interfaces;
         while(temp_addr != NULL) {
@@ -58,7 +57,7 @@ RCT_EXPORT_METHOD(getIPAddress:(RCTResponseSenderBlock)callback)
             temp_addr = temp_addr->ifa_next;
         }
     }
-    
+
     freeifaddrs(interfaces);
     callback(@[address]);
 }
